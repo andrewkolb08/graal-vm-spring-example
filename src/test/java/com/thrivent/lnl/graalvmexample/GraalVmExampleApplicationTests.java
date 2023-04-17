b40250abeda002cdea5d69571235b779345c3795
Andrew Kolb <andrew.kolb@thrivent.com>
2023-04-17 12:02:44 -0500

    Add Maven docker image buildling

---
+++ src/test/java/com/thrivent/lnl/graalvmexample/GraalVmExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrivent.lnl.graalvmexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalVmExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
